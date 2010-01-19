5798decc536d2c3ba4f2bcb616c26fb81c6689af
Andreas Wenger <andi@xenoage.com>
2010-01-19 14:53:09 +0100

    Script to copy from developer rep to public rep

---
+++ copy-from-developer-rep.sh
@@ -0,0 +1,16 @@
+rm -rf core
+rm -rf lib
+rm -rf midi-out
+rm -rf musicxml-in
+rm -rf player
+rm -rf shared
+rm -rf util
+rm -rf viewer
+cp -r ../Zong/Programm/Zong/core .
+cp -r ../Zong/Programm/Zong/lib .
+cp -r ../Zong/Programm/Zong/midi-out .
+cp -r ../Zong/Programm/Zong/musicxml-in .
+cp -r ../Zong/Programm/Zong/player .
+cp -r ../Zong/Programm/Zong/shared .
+cp -r ../Zong/Programm/Zong/util .
+cp -r ../Zong/Programm/Zong/viewer .
